c6f8e8fbea58bd48e4246e94fb90b525f4a6fc96
emelda <emelda.perez@moringaschool.com>
2021-09-05 11:59:26 +0300

    Add arrays containing male and female Akan names.

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-var days=("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday")
+var days=("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
+var males=("Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","kwame");
+var females=("Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama")
