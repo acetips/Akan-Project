ca8b9b874ab5ecf373833a570096b6ebb7cd62ce
emelda <emelda.perez@moringaschool.com>
2021-09-05 12:11:50 +0300

    Add the .getElementById method to identify individual elements.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 var days=("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
 var males=("Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","kwame");
 var females=("Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama")
+var yearofbirth=document.getElementById("yearofbirth")
+var monthofbirth=document.getElementById("monthofbirth")
+var dateofbirth=document.getElementById("dateofbirth")
